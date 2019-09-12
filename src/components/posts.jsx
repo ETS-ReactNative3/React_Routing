8cb2cdc952c4d7489174d6afcf16d8c36b6a651c
Destros <patrick.fallmann@hotmail.com>
2019-09-12 21:58:34 +0200

    Installed queryString and tested it with the posts.jsx component

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  // This creates an object with properties based on the parameters in the query string.
+  const result = queryString.parse(location.search);
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
